020bf66b8612c4815d3197047d36727703f8f472
Aristana Scourtas <ascourtas@gmail.com>
2020-06-10 18:42:14 -0400

    add Pearson's r calc using Pandas

--- scripts/stats.py
+++ scripts/stats.py
@@ -6,8 +6,9 @@ import scipy
 import pandas as pd
 
 
-def calc_correlation(x1, x2):
-    pass
+def calc_correlation(joined_df):
+    r = joined_df[["HARDSHIP INDEX", "TRANSIT SCORE"]].corr(method="pearson")
+    return r
 
 def dataframe_diff(df1, df2):
     temp_df = df1.merge(df2, indicator=True, how='outer')
@@ -18,11 +19,11 @@ def main():
     hardship_df = pd.read_csv("../data/Census_Data_-_Selected_socioeconomic_indicators_in_Chicago__2008___2012.csv")
     transit_df = pd.read_csv("../data/transit-score.csv")
 
-    diff_df = dataframe_diff(hardship_df, transit_df)
+    # diff_df = dataframe_diff(hardship_df, transit_df)
 
     joined_df = hardship_df.merge(transit_df, on=["COMMUNITY AREA NAME"], how="inner")
 
-    calc_correlation(hardship_df["HARDSHIP INDEX"], transit_df["TRANSIT SCORE"])
+    r = calc_correlation(joined_df)
 
 if __name__ == "__main__":
     main()
