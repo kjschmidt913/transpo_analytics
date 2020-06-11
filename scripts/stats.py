b0c3ebbfb914aecff1a0b150305f4d9d35a4909d
Aristana Scourtas <ascourtas@gmail.com>
2020-06-10 22:10:22 -0400

    update pearson r to work for all hardship features

--- scripts/stats.py
+++ scripts/stats.py
@@ -2,13 +2,27 @@
 #
 # last modified 6/10/20 by Aristana Scourtas
 
-import scipy
+from scipy.stats import pearsonr
 import pandas as pd
 
 
 def calc_correlation(joined_df):
-    r = joined_df[["HARDSHIP INDEX", "TRANSIT SCORE"]].corr(method="pearson")
-    return r
+    # r = joined_df[["HARDSHIP INDEX", "TRANSIT SCORE"]].corr(method="pearson")
+    hardship_cols = ['HARDSHIP INDEX', 'PERCENT OF HOUSING CROWDED', 'PERCENT HOUSEHOLDS BELOW POVERTY',
+       'PERCENT AGED 16+ UNEMPLOYED',
+       'PERCENT AGED 25+ WITHOUT HIGH SCHOOL DIPLOMA',
+       'PERCENT AGED UNDER 18 OR OVER 64', 'PER CAPITA INCOME ']
+
+    print("VARIABLES \t r \t p \n")
+    for col in hardship_cols:
+        clean_df = joined_df[[col, "TRANSIT SCORE"]].dropna()
+        r, p = pearsonr(clean_df["TRANSIT SCORE"], clean_df[col])
+        print("TRANSIT SCORE vs {} \t {} \t {}".format(col, r, p))
+        # clean_df.to_csv("temp.csv")
+
+# def calc_correlation(x1, x2):
+#     r, p = pearsonr(x1, x2)
+#     return (r, p)
 
 def dataframe_diff(df1, df2):
     temp_df = df1.merge(df2, indicator=True, how='outer')
@@ -23,7 +37,9 @@ def main():
 
     joined_df = hardship_df.merge(transit_df, on=["COMMUNITY AREA NAME"], how="inner")
 
-    r = calc_correlation(joined_df)
+    # r, p = calc_correlation(joined_df["TRANSIT SCORE"], joined_df["HARDSHIP INDEX"])
+
+    calc_correlation(joined_df)
 
 if __name__ == "__main__":
     main()
